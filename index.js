5b12e2c20fa55ac89d87be335c3a314fa4fefe24
Your Nameabubakarsiddik-1259 <abubakarsiddik1259@gmail.com>
2026-05-15 22:48:06 +0600

    Latest quntity foods api add

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.rfyh9vd.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("foods_db");
+    const foodsCollection = db.collection("foods");
+
+    const foodRequestCollection = db.collection("foodRequests");
+
+    //.....latest products..........
+    app.get("/latest-foods", async (req, res) => {
+      const cursor = await foodsCollection
+        .find()
+        .sort({ food_quantity: -1 })
+        .limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
